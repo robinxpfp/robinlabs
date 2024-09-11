724528522f64a9993c8246515306649428db34b7
robinxpfp <elmoxme@proton.me>
2024-09-11 03:14:12 -0400

    wip

--- src/app/about/profile.tsx
+++ src/app/about/profile.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function Profile() {
   return (
-    <div>
-      {/* <div className="w-full max-w-xs mx-auto"></div> */}
+    <div className="w-full h-auto max-w-[300px] min-w-[250px] min-h-[250px] relative">
       <Image
         className="rounded-sm"
+        layout="responsive"
         width={300}
         height={300}
+        objectFit="cover"
         alt="PFP"
-        // src={"/pfp-l.jpg"}
-        src={"/pfp.png"}
+        src={"/pfp.jpg"}
       />
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="max-w-3xl mx-auto flex flex-col justify-center min-h-screen mb-20">
       <Nav />
-      <div className="flex sm:flex-col flex-col-reverse justify-center  sm:items-end items-center my-12 max-w-xl sm:mx-auto mx-5 sm:-space-y-20">
+      <div className="flex sm:flex-col flex-col-reverse justify-center  sm:items-end items-center my-12 max-w-xl sm:mx-auto mx-5 sm:-space-y-32">
         <div className="flex justify-start items-start">
           <Profile />
         </div>
